ac87d5e198bfa57004b1900a118d5deb63c2aae8
Sébastien <seb.lasante@hotmail.com>
2021-01-07 16:41:41 +0100

    fini

---
+++ consignes1.md
@@ -0,0 +1,9 @@
+- # Exo 1 
+  - ## Créer une variable qui recoit un jour de la semaine via un prompt.
+  - ## A l'aide d'un switch, affichez ce que vous devez faire pendant le jour que vous avez choisi:
+
+    - ### Ex: On est lundi. Tu as cours a MG et en soirée tu as sport.
+
+- # Exo 2 
+  - ## Créer une variable qui recoit ton age via un prompt.
+  - ## A l'aide d'un switch, affichez si vous avez plus ou moins de 18 ans.
---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+// let jour = prompt("le jour de la semaine").toLocaleLowerCase();
+
+// switch (jour){
+//     case "vendredi":
+//     console.log("je mange a midi");
+//     break;
+// }
+
+// let age = prompt("20 ans")
+
+// switch ("age"){
+//     case "age":
+//         console.log("j'ai deux en plus de 18 ans")
+//         break;
+// }
+
+
+// Exercice 1
+
+//1 
+// let jSemaine = prompt('Le jour de la semaine?').toLowerCase();
+
+// switch (jSemaine) {
+//     case "lundi": case "mardi": case "mercredi" :
+//         console.log(`${jSemaine} MolenGeek`);
+//         break;
+//     case "jeudi": case "vendredi":
+//         console.log(`${jSemaine} elearning`);
+//         break;
+//     case "samedi": case "dimanche":
+//         console.log('maison');
+//         break;
+//     default:
+//         console.log('Error, reconnais pas le jour');
+//         break;
+// }
+
+//2
+
+// let age = parseInt(prompt('Entrez votre âge?'));
+
+// switch (isNaN(age)) {
+//     case true:
+//         console.log(`Error n'est pas un nombre`);
+//         break;
+//     case false:
+//         switch (age >=18 ) {
+//             case true:
+//                 console.log("Il a plus de 18ans");
+//                 break;
+//             case false:
+//                 console.log("ll a moins de 18ans");
+//         }
+//         break;
+// }
